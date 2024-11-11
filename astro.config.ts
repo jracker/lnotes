d48d35eb760966410dc3211cd32956eef77c73b4
jracker <48454347+jracker@users.noreply.github.com>
2024-11-11 20:20:46 -0300

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -95,7 +95,8 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
+	site: "https://jracker.github.io/",
+  base: "/lnotes"
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
